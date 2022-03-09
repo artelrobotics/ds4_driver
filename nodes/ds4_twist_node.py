fa47a2548648c423051772c6d62bd4c3f9323f91
nvidia <sahboztursunbaev@gmail.com>
2022-03-09 16:24:24 +0500

    updated

--- nodes/ds4_twist_node.py
+++ nodes/ds4_twist_node.py
@@ -14,14 +14,17 @@ class StatusToTwist(object):
         else:
             self._cls = Twist
         self._inputs = rospy.get_param("~inputs")
-        self._scales = rospy.get_param("~scales")
-
+        # self._scales = rospy.get_param("~scales")
+        self.scale = 0.1
+        self.last_state_l2 = 1
+        self.last_state_r2 = 1
+        self.msg = Status()
         self._attrs = []
         for attr in Status.__slots__:
             if attr.startswith("axis_") or attr.startswith("button_"):
                 self._attrs.append(attr)
 
-        self._pub = rospy.Publisher("cmd_vel", self._cls, queue_size=1)
+        self._pub = rospy.Publisher("driver/cmd_vel", self._cls, queue_size=1)
         rospy.Subscriber("status", Status, self.cb_status, queue_size=1)
 
     def cb_status(self, msg):
@@ -30,10 +33,21 @@ class StatusToTwist(object):
         :type msg: Status
         :return:
         """
+        self.msg = msg
         input_vals = {}
         for attr in self._attrs:
             input_vals[attr] = getattr(msg, attr)
-
+        
+        if msg.button_r2 and not self.last_state_r2:
+            if self.scale < 1.1:
+                self.scale += 0.1
+            
+        if msg.button_l2 == 1 and self.last_state_l2 == 0:
+            if self.scale > 0.2:
+                self.scale -= 0.1
+        
+        self.last_state_l2 = msg.button_l2
+        self.last_state_r2 = msg.button_r2
         to_pub = self._cls()
         if self._stamped:
             to_pub.header.stamp = rospy.Time.now()
@@ -45,12 +59,12 @@ class StatusToTwist(object):
         for vel_type in self._inputs:
             vel_vec = getattr(twist, vel_type)
             for k, expr in self._inputs[vel_type].items():
-                scale = self._scales[vel_type].get(k, 1.0)
+                # scale = self._scales[vel_type].get(k, 1.0)
+
                 val = eval(expr, {}, input_vals)
-                setattr(vel_vec, k, scale * val)
+                setattr(vel_vec, k, self.scale * val)
 
         self._pub.publish(to_pub)
-
 
 def main():
     rospy.init_node("ds4_twist")
